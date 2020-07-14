85a92fcdf2b4552959334c8eeb0fd22f992cb635
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-13 23:31:44 -0400

    dylan

---
+++ code_here.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+bob_dylan_albums = [
+    {title: "The Freewheelin' Bob Dylan", tracks: 13, year: 1963},
+    {title: "The Times They Are A-Changin'", tracks: 10, year: 1964},
+    {title: "Another Side of Bob Dylan", tracks: 11, year: 1964},
+    {title: "Bringing It All Back Home", tracks: 11, year: 1965},
+    {title: "Highway 61 Revisited", tracks: 9, year: 1965}
+]
+
+
+def run(array)
+    puts "Hello! Welcome to the Bob Dylan partial album information center."
+    puts "What would you like to do?"
+        puts "Press 1 to see a list of albums"
+        puts "Press 2 to find out the year of an album"
+        puts "Press 3 to find an album(s) by year"
+
+    input = gets.chomp
+
+    if input == "1"
+        array.map do |album|
+            album[:title]
+        end
+    elsif input == "2"
+        puts "Which album?"
+        name = gets.chomp
+        array.find do |album|
+            album[:title] == name
+            album[:year]
+        end
+    elsif input == "3"
+        puts "Which year?"
+        year = gets.chomp
+        array.collect do |album|
+            album[:year] == year
+        end
+    end
+end
+
+run(bob_dylan_albums)
